9402d1492b411976b9b3bc783c68b882d7dc2dd3
gwillz <me@gwilyn.com>
2020-09-20 13:42:30 +0930

    Added more test utilities.

--- tests/test.ts
+++ tests/test.ts
@@ -1,15 +1,55 @@
 
-import { Test } from 'tape';
+import { Test, TestCase } from 'tape';
 
-type TestCb = (test: Test) => Promise<void>;
+type AsyncTestCase = (test: AsyncTest) => Promise<void>;
 
-export function a(cb: TestCb): TestCb {
+interface AsyncTest extends Test {
+    aThrows: (this: AsyncTest, fn: () => Promise<void>) => Promise<void>;
+    aDoesNotThrow: (this: AsyncTest, fn: () => Promise<void>) => Promise<void>;
+}
+
+
+export function a(cb: AsyncTestCase): TestCase {
     return async (test) => {
+        // @ts-expect-error
+        test.aThrows = asyncThrows.bind(test);
+        // @ts-expect-error
+        test.aDoesNotThrow = asyncDoesNotThrow.bind(test);
+        
         try {
-            await cb(test);
+            await cb(test as AsyncTest);
         }
         catch (error) {
             test.end(error);
         }
     }
+}
+
+
+async function asyncThrows(this: Test, cb: () => Promise<void>, msg?: string): Promise<void> {
+    try {
+        await cb();
+        this.fail(msg || 'Should throw');
+    }
+    catch (error) {}
+}
+
+
+async function asyncDoesNotThrow(this: AsyncTest, cb: () => Promise<void>, msg?: string): Promise<void> {
+    try {
+        await cb();
+    }
+    catch (error) {
+        console.error(error);
+        this.fail(msg || 'Should not throw');
+    }
+}
+
+
+export async function toArray<T>(generator: AsyncGenerator<T>): Promise<T[]> {
+    const list: T[] = [];
+    for await (let item of generator) {
+        list.push(item);
+    }
+    return list;
 }
