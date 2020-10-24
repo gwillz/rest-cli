76ec42eeb21b7077f00747f9d5c5b43dce386b2e
gwillz <me@gwilyn.com>
2020-10-24 18:13:04 +1030

    Some docs for variable map.

--- src/VarMap.ts
+++ src/VarMap.ts
@@ -12,14 +12,33 @@ type Props = {
     entities?: EntityMap;
 }
 
+/** Match double brackets. */
 const RE = /{{([^}]+)}}/g;
+
+/** Match $function and args. */
 const FUNCTION_RE = /^\$([^\s]+)\s?(.*)/;
+
+/** Match named request/response headers. */
 const HEADER_RE = /^([^.]+)\.(request|response)\.headers\.(.+)/;
+
+/** Match named request/response body paths, supports jsonpath + xpath. */
 const BODY_RE = /^([^.]+)\.(request|response)\.body\.?((\$|\/\/)?[^}]+)?/;
 
+
+/**
+ * This holds local variables and entities.
+ * Primarily though, it does all the fancy template interpolation stuff.
+ */
 export class VarMap {
     
+    /**
+     * Variables are user defined `@thing = whatever`.
+     */
     variables: StringMap;
+    
+    /**
+     * Entities are request/response objects from `# @name` declarations.
+     */
     entities: EntityMap;
     
     constructor(props?: Props) {
@@ -27,26 +46,57 @@ export class VarMap {
         this.entities = props?.entities ?? {};
     }
     
+    /**
+     * Add a bunch of local variables.
+     * 
+     * @param variables 
+     */
     public addVars(variables: StringMap) {
         for (let [name, value] of Object.entries(variables)) {
             this.addVar(name, value);
         }
     }
     
+    /**
+     * Add a single variable.
+     * 
+     * @param name 
+     * @param value 
+     */
     public addVar(name: string, value: string) {
         this.variables[name] = value;
     }
     
+    /**
+     * Add a named request/response entity.
+     * 
+     * Although if you pass an unnamed entity, we'll just ignore it.
+     * 
+     * @param entity 
+     */
     public addEntity(entity: Entity) {
         if (!entity.name) return;
         this.entities[entity.name] = entity;
     }
     
+    /**
+     * Interpolate variables into this body.
+     * 
+     * @param text A body payload.
+     * @return A copy of the body with inserted variables.
+     */
     public replace(text: string): string {
+        // Interpolate our own variables before 
         const locals = this.replaceMap(this.variables);
         return this._replace(text, locals);
     }
     
+    /**
+     * Interpolate variables into a variable map.
+     * 
+     * @param map A variable map.
+     * @return A copy of the map with inserted variables.
+     */
     public replaceMap(map: StringMap): StringMap {
         const copy: StringMap = {};
         
@@ -57,6 +107,12 @@ export class VarMap {
         return copy;
     }
     
+    /**
+     * Interpolate variables into a headers map.
+     * 
+     * @param headers A set of headers.
+     * @return A copy of the headers with inserted variables.
+     */
     public replaceHeaders(headers: Headers): Headers {
         const copy = new Headers();
         
@@ -64,6 +120,9 @@ export class VarMap {
             copy.append(name, this.replace(value));
         }
         
+        // The 'authorization' header is a special case.
+        // We do automatic basic auth encoding if the user passes a
+        // "basic <user> <pass>" pattern.
         const auth = copy.get("authorization");
         if (auth) {
             const m = /^basic\s+([^\s]+)\s+(.+)/i.exec(auth);
@@ -78,6 +137,16 @@ export class VarMap {
         return copy;
     }
     
+    /**
+     * Find and replace request/response header interpolations.
+     * 
+     * E.g.
+     * - 'name.response.header.content-type' => 'application/x-www-urlencoded'
+     * - 'name.request.header.accepts' => 'application/json'
+     * 
+     * @param content Template bracket contents.
+     * @return The matching header value, null if no match or invalid signature.
+     */
     private _findHeader(content: string) {
         const m = HEADER_RE.exec(content);
         
@@ -94,6 +163,15 @@ export class VarMap {
         return "";
     }
     
+    /**
+     * Find and replace function interpolations.
+     * 
+     * E.g.
+     * - '$datetime iso8601 d 3' => '2020-01-04T23:59:59.999Z'
+     * 
+     * @param content Template bracket contents.
+     * @return The function result.
+     */
     private _findFunction(content: string) {
         const m = FUNCTION_RE.exec(content);
         
@@ -111,6 +189,17 @@ export class VarMap {
         return "";
     }
     
+    /**
+     * Find and replace request/response body interpolations.
+     * 
+     * E.g.
+     * - 'name.request.body.$..thing_id' => '123'
+     * - 'name.response.body.$.created_date' => '2020-12-30'
+     * - 'name.request.body.//path/to/etc' => 'hello there'
+     * 
+     * @param content Template bracket contents.
+     * @return The path result.
+     */
     private _findBody(content: string) {
         const m = BODY_RE.exec(content);
         
@@ -133,6 +222,7 @@ export class VarMap {
                     
                     const value = JSONPath({ path, json, wrap: false });
                     
+                    // Can't have [object Object], gotta be JSON strings.
                     if (typeof value === 'object') {
                         return JSON.stringify(value);
                     }
@@ -160,6 +250,15 @@ export class VarMap {
         return "";
     }
     
+    /**
+     * Internal interpolator.
+     * 
+     * Throw any thing that is wrapped in {{}} double brackets at this.
+     * It'll figure out whether it's a header, body, function or variable.
+     * 
+     * @param text The template.
+     * @param locals The variable map for interpolation.
+     */
     private _replace(text: string, locals: StringMap): string {
         
         return text.replace(RE, (_: string, content: string) => {
